2527c2b9e113581627953a8c291faf91b7a43e43
Aarno Aukia <aarno.aukia@vshn.ch>
2015-12-11 16:11:03 +0100

    install neobundle on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,10 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
+	NEOBUNDLE=$(mktemp /tmp/dotfiles.XXXXXX)
+	curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > $NEOBUNDLE
+	sh $NEOBUNDLE
+	rm $NEOBUNDLE
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
