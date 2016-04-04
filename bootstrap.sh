4a606002e7664cfcddbf16904904af04dd85a6d0
Aarno Aukia <aarno@aukia.com>
2016-04-04 09:45:18 +0300

    add --diff/-d option to show difference between git and installed files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+RSYNCCMD="rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+      --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~"
+
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+#git pull origin master;
+
+function diffIt() {
+  FILES=$($RSYNCCMD -n | grep -v "building file list" | grep -v "sent .* bytes" | grep -v "total size is ") #run in dry-run mode and list the files
+  for f in $FILES; do
+    if [ -f "$f" ] ; then
+      diff -urN ./$f ~/$f
+    fi
+  done
+}
 
 function doIt() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-    --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+  $RSYNCCMD
   source ~/.bash_profile;
   # bootstrap vim neobundle
   if [ ! -d ~/.vim/bundle/neobundle.vim ] ; then
@@ -17,7 +28,9 @@ function doIt() {
   fi
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" == "--diff" -o "$1" == "-d" ]; then
+  diffIt;
+elif [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt;
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
@@ -26,4 +39,6 @@ else
     doIt;
   fi;
 fi;
+unset RSYNCCMD;
 unset doIt;
+unset diffIt;
